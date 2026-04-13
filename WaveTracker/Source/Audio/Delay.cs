4dd33bf9ac270cb5f74b8ff3e0ccbc74b22427f3
andrz <andrz@LAPTOP-VQ95CQRV>
2026-04-13 22:37:46 +0200

    Optimized delay

--- WaveTracker/Source/Audio/Delay.cs
+++ WaveTracker/Source/Audio/Delay.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WaveTracker.Audio {
     public class Delay {
 
-        List<float> bufferL;
-        List<float> bufferR;
+        float[] bufferL;
+        float[] bufferR;
 
         int positionL = 0;
         int positionR = 0;
@@ -12,23 +13,17 @@ namespace WaveTracker.Audio {
         int delayInSamples = (int)(44100 * 0f);
 
         public Delay() {
-            bufferL = new List<float>(44100 * 10);
-            bufferR = new List<float>(44100 * 10);
-            for (int i = 0; i < bufferL.Capacity; i++) {
-                bufferL.Add(0);
-                bufferR.Add(0);
-            }
+            bufferL = new float[44100 * 10];
+            bufferR = new float[44100 * 10];
         }
 
-        public void SetParams(double delaySeconds, float decay) {
+        public void SetParams(double delay, float decay) {
             this.decay = decay/16f;
-            delayInSamples = (int)((double)(delaySeconds/16f) * 44100);
+            delayInSamples = (int)((delay/16f) * 44100);
             positionL = 0;
             positionR = 0;
-            for (int i = 0; i < bufferL.Capacity; i++) {
-                bufferL[i] = 0;
-                bufferR[i] = 0;
-            }
+            Array.Clear(bufferL);
+            Array.Clear(bufferR);
         }
 
         public void Transform(int playbackPosition, float inputL, float inputR, out float outputL, out float outputR) {
@@ -36,13 +31,13 @@ namespace WaveTracker.Audio {
             outputL = delayInSamples == 0 ? inputL : Apply(bufferL, inputL, ref positionL);
             outputR = delayInSamples == 0 ? inputR : Apply(bufferR, inputR, ref positionR);
 
-            float Apply(List<float> buffer, float sample, ref int position)
+            float Apply(float[] buffer, float sample, ref int position)
             {
                 buffer[position] = sample;
-                int pos = mod((position - delayInSamples), buffer.Count);
+                int pos = mod((position - delayInSamples), buffer.Length);
                 var delayedSample = buffer[pos];
                 buffer[position] = (delayedSample * decay) + sample;
-                position = ((position + 1) % buffer.Count);
+                position = ((position + 1) % buffer.Length);
                 return (delayedSample * decay) + sample;
             }
 
